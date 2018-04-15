b4c115b35b8fdbc9360ce60fde8358dde24f983b
李梦龙 <649076408@qq.com>
2018-04-16 02:34:02 +0800

    chore($detail): improve code indentation, detail

--- example/App.vue.d.ts
+++ example/App.vue.d.ts
@@ -1,4 +1,4 @@
 import Vue from 'vue';
 export default class App extends Vue {
-    message: string;
+  message: string;
 }
--- src/component/Hello.vue.d.ts
+++ src/component/Hello.vue.d.ts
@@ -1,4 +1,4 @@
 import { Vue } from 'vue-property-decorator';
 export default class HelloDecorator extends Vue {
-    name: string;
+  name: string;
 }
